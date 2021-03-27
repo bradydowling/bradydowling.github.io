09bd8e92a6f31085685272b935fdcb53a03ddf62
bradydowling <bradydowling@gmail.com>
2021-03-27 07:51:22 -0400

    Add API call

--- browser-devtools-workshop/index.js
+++ browser-devtools-workshop/index.js
@@ -7,3 +7,19 @@ console.log("Welcome to the page");
 console.log(`WHS stands for ${theW} high school`);
 
 console.timeEnd("timer");
+
+// https://jsonplaceholder.typicode.com/posts
+
+function checkDataItem(item) {
+  if (item.body.indexOf("architect") > -1) {
+    console.error("Some bad stuff happened");
+  }
+}
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(response => response.json())
+  .then(data => {
+    for (let i = 0; i < data.length; i++) {
+      checkDataItem(data[i]);
+    }
+  })
